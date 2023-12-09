fcb8cf291cb5c236d45ab4ad4011f25c5ed245b7
GorNalbandyan <gor.ar.nalb@gmail.com>
2023-12-10 01:05:38 +0400

    API testing project is created

---
+++ GMapAPITesting/GoogleSignUpTextFixture.cs
@@ -0,0 +1,25 @@
+﻿using Google.Maps;
+using NUnit.Framework;
+using GoogleMapsTesting.Helpers;
+
+namespace GMapAPITesting
+{
+    [Parallelizable(ParallelScope.All), TestFixture]
+    public class GoogleSignUpTextFixture
+    {
+        [SetUp]
+        public void GoogleSignedSetUp()
+        {
+            try
+            {
+                var myAPIKey = new SettingsHelper().GoogleAPIKey;
+                GoogleSigned.AssignAllServices(new GoogleSigned(myAPIKey));
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail($"Couldn't set up Google Signed beacuse of the error: {ex.Message}");
+            }
+            
+        }
+    }
+}
